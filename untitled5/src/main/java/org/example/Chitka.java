86e299bc29dee43a5ea5eb4a2834b4aa414f175c
Krasekkech <kaml-03@mail.ru>
2022-12-29 14:28:32 +0300

    добавили функцию поиска повторений слов

--- untitled5/src/main/java/org/example/Chitka.java
+++ untitled5/src/main/java/org/example/Chitka.java
@@ -5,6 +5,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Set;
 
 public class Chitka {
     public static void main(String[] args) {
@@ -15,15 +17,32 @@ public class Chitka {
             str = new String(en);
         } catch (IOException e){
             e.printStackTrace();
-           }
+        }
         Scanner newstr = new Scanner(str);
         int wordcount = 0; //обнулили кол-во слов
         
-        //while (str.hasNextLine()){
+        while (newstr.hasNextLine()){
             String[] array = newstr.nextLine().split(" "); // добавление каждого слова в массив
             wordcount = wordcount + array.length;
         }
-        //System.out.println(wordcount);
+        System.out.println(wordcount);
+
+        HashMap<String, Integer> map = new HashMap<>();
+        String array2[] = str.split(" ");
+        for (String s : array2){
+            if (map.containsKey(s)){
+                map.put(s, map.get(s) + 1);
+            }
+            else{
+                map.put(s,1);
+            }
+
+        }
+        Set<String> keys = map.keySet();
+        for (String key : keys) {
+            System.out.println(key + " " + "-" + " " + map.get(key));
+            //System.out.println(map.get(key));
+        }
     }
 
 }
